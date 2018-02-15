644f980b13f7c017ff38fa3d4bc819a8b3f57bde
Mikkel Sandberg <mikkel.h.sandberg@gmail.com>
2018-02-15 14:53:00 -0800

    refactoring code to generate boards of any size and generate bomb board

--- minesweeper.js
+++ minesweeper.js
@@ -1,20 +1,54 @@
-const board = [
-	[' ', ' ', ' '],
-	[' ', ' ', ' '],
-	[' ', ' ', ' ']
-];
+'use strict';
+
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+	const board = [];
+
+	for (let rowsIndex = 0; rowsIndex < numberOfRows; rowsIndex++) {
+		const row = [];
+		for (let columnsIndex = 0; columnsIndex < numberOfColumns; columnsIndex++) {
+			row.push(' ');
+		}
+		board.push(row);
+	}
+
+	return board;
+};
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+	const board = [];
+
+	for (let rowsIndex = 0; rowsIndex < numberOfRows; rowsIndex++) {
+		const row = [];
+		for (let columnsIndex = 0; columnsIndex < numberOfColumns; columnsIndex++) {
+			row.push(null);
+		}
+		board.push(row);
+	}
+
+	let numberOfBombsPlaced = 0;
+
+	while (numberOfBombsPlaced < numberOfBombs) {
+		let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+		let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+
+		board[randomRowIndex][randomColumnIndex] = 'B';
+
+		numberOfBombsPlaced++;
+
+		// need to update to account for situation where new bomb is added
+		// to space where bomb has already been placed
+	}
+
+	return board;
+};
 
 const printBoard = board => {
-	console.log('Current Board:');
+	console.log(board.map(row => row.join(' | ')).join('\n'));
+};
 
-	for (let i = 0; i < board.length; i++) {
-		console.log(board[i].join(' | ') + '\n');
-	}
-}; 
+const playerBoard = generatePlayerBoard(3, 4);
 
-printBoard(board);
+const bombBoard = generateBombBoard(3, 4, 5);
 
-board[0][1] = '1';
-board[2][2] = 'B';
-
-printBoard(board);
+console.log(`Player Board:\n${printBoard(playerBoard)}`);
+console.log(`Bomb Board:\n${printBoard(bombBoard)}`);
